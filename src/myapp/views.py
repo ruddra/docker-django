cdc5cef92893c8a7a16c47c23c5f0d567a818ddb
Serkan Sökmen <e.serkan.sokmen@gmail.com>
2017-05-14 12:39:17 +0300

    Removed unncessary mailing code

--- src/myapp/views.py
+++ src/myapp/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
+from .tasks import show_hello_world
 
 # Create your views here.
+
+class ShowHelloWorld(TemplateView):
+    template_name='hello_world.html'
+
+    def get(self, *args, **kwargs):
+        show_hello_world.apply()
+        return super().get(*args, **kwargs)
--- src/mydjango/urls.py
+++ src/mydjango/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
+
+from myapp.views import ShowHelloWorld
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', ShowHelloWorld.as_view())
 ]
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
